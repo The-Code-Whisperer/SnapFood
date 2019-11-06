75dba0fd1da6d3e9e1a889499e60b6c84a3db601
The-Code-Whisperer <hansomekim@gmail.com>
2019-11-06 23:11:16 +0000

    initial commit

--- application.py
+++ application.py
@@ -5,6 +5,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Index route
-app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST"
+        pass
+    else:
+        return render_template("register.html")
